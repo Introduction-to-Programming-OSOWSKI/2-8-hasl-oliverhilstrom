2d30ecca2bc8be95acfe1738475946b354b1fb1f
oliverhilstrom <819492@gmail.com>
2021-10-04 15:19:37 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #WRITE YOUR CODE IN THIS FILE
+def hasL(w):
+    x = 0
+    while x < len(w):
+        if w == "l":
+            return True
+        else:
+            return False
+        x = x + 1
+print(hasL("alligator"))
