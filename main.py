155d6379228309b90323ddc68541436ead0eebd7
oliverhilstrom <819492@gmail.com>
2021-10-05 14:59:42 -0500

    3

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 #WRITE YOUR CODE IN THIS FILE
 def hasL(w):
+    w = w.lower()
     for i in range(0, len(w)):
-        print(w[i])
-        if i == "l":
+        if w[i] == "l":
             return True
-        else:
-            return False
+        
 hasL("hassle")
